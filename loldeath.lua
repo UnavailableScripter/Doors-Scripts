2f4cf1d4790fc401a0e5477d2622bdc2c51e4896
UnavailableScripter <128002849+UnavailableScripter@users.noreply.github.com>
2023-04-15 10:18:21 -0400

    Update loldeath.lua

--- loldeath.lua
+++ loldeath.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/UnavailableScripter/Sources/main/lolSOURCE.lua?token=GHSAT0AAAAAACADACWTHWEQHI5RJ3FYH5A2ZBYTNNA'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/UnavailableScripter/Sources/main/lolSOURCE.lua'))()
