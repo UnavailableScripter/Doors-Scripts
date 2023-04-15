9f3b85c32315d416f94357973839552c789c70ec
UnavailableScripter <128002849+UnavailableScripter@users.noreply.github.com>
2023-04-15 15:11:40 -0400

    Update and rename loldeath.lua to CustomDeathMessage.lua

---
+++ CustomDeathMessage.lua
@@ -0,0 +1,4 @@
+customMessage = {"Message1", "Message2"}
+Type = "Guiding Light"
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/UnavailableScripter/Sources/main/customDeathMessageSource.lua'))()
--- loldeath.lua
+++
@@ -1,1 +0,0 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/UnavailableScripter/Sources/main/lolSOURCE.lua'))()
