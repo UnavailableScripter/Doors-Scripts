fbc961a27ec1e5ea12a198546f0dba5be389fb49
UnavailableScripter <128002849+UnavailableScripter@users.noreply.github.com>
2023-04-15 15:32:27 -0400

    Update CustomDeathMessage.lua

--- CustomDeathMessage.lua
+++ CustomDeathMessage.lua
@@ -1,4 +1,14 @@
-customMessage = {"Message1", "Message2"}
-Type = "Guiding Light"
+local customMessage = {"Message3", "Message2", "These messages display in order."}
+local Type = "Guidinging Light" -- Change to Curious Light to have Curious Light layout
+local activeType = nil
 
-loadstring(game:HttpGet('https://raw.githubusercontent.com/UnavailableScripter/Sources/main/customDeathMessageSource.lua'))()
+if Type = "Guiding Light" then
+	activateType = "Blue"
+elseif Type = "Curious Light" then
+	activateType = "Yellow"
+end
+
+game.Players.LocalPlayer.Character.Humanoid.Died:Connect(function()
+wait(1.2)
+firesignal(game.ReplicatedStorage.EntityInfo.DeathHint.OnClientEvent, customMessage, activateType) end)
+wait(300)
