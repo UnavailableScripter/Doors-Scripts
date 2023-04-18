96aedc040d065935711ab6050c21ec3f3d3be8a2
UnavailableScripter <128002849+UnavailableScripter@users.noreply.github.com>
2023-04-18 15:44:24 -0400

    Update and rename crucifixinRift.lua to crucifixforanythinginRift.lua

--- crucifixforanythinginRift.lua
+++ crucifixforanythinginRift.lua
@@ -1,2 +1,2 @@
---Also works on anything btw
+-- doesnt work on custom spawned entities.
 loadstring(game:HttpGet('https://raw.githubusercontent.com/UnavailableScripter/Sources/main/CrucInRiftSOURCE.lua'))()
